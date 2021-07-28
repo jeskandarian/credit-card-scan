9aa39f44877b731b84f6144f7d4970f33be89e3e
Jay Eskandarian <jay@eskandarian.com>
2021-07-28 15:46:02 -0700

    Initial Commit

---
+++ CreditCardScan/CreditCardScanApp.swift
@@ -0,0 +1,17 @@
+//
+//  CreditCardScanApp.swift
+//  CreditCardScan
+//
+//  Created by Jay Eskandarian on 7/28/21.
+//
+
+import SwiftUI
+
+@main
+struct CreditCardScanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
